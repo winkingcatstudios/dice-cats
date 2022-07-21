6d564e2f6d39f8c43364effa5693343713eb72a4
winkingcat <dankercher@gmail.com>
2022-07-20 21:48:07 -0400

    navlinks and header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
- 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
- 
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+
 const App = () => {
   return (
-   <Router>
-     <Routes>
-       <Route path="/" element={<Users/>}/>
-       <Route path="/places/new" element={<NewPlace/>}/>
-     </Routes>
-   </Router>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
+    </Router>
   );
 };
- 
+
 export default App;
